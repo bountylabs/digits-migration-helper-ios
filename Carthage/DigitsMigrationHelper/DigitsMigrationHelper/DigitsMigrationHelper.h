81717547cb145859dae150c24323b1f474df17ea
Christian Niles <cniles@twitter.com>
2018-03-23 15:20:33 -0700

    Add support for Carthage

---
+++ Carthage/DigitsMigrationHelper/DigitsMigrationHelper/DigitsMigrationHelper.h
@@ -0,0 +1,24 @@
+//
+//  DigitsMigrationHelper.h
+//  DigitsMigrationHelper
+//
+//  Created by Christian Niles on 3/23/18.
+//  Copyright © 2018 Google Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DigitsMigrationHelper.
+FOUNDATION_EXPORT double DigitsMigrationHelperVersionNumber;
+
+//! Project version string for DigitsMigrationHelper.
+FOUNDATION_EXPORT const unsigned char DigitsMigrationHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DigitsMigrationHelper/PublicHeader.h>
+
+
+#import <DigitsMigrationHelper/Keychain.h>
+#import <DigitsMigrationHelper/KeychainItem.h>
+#import <DigitsMigrationHelper/Migrator.h>
+#import <DigitsMigrationHelper/Session.h>
+#import <DigitsMigrationHelper/SessionStore.h>
